3582927be69420f9c9cb532c80c3ea03e696b374
hebidu <hebiduhebi@126.com>
2017-06-05 16:55:05 +0800

    init vendor

---
+++ src/test/TestVendor.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2017-06-05
+ * Time: 16:54
+ */
+
+namespace itboye\vendor;
+
+/**
+ * Class TestVendor
+ * 测试类
+ * @package itboye\vendor
+ */
+class TestVendor
+{
+    public function test(){
+        return  "test";
+    }
+}
